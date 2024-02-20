ca6dbf91a869e215bcccd144bc5552cbb475b3d2
Felipe Barraza <felipe.barraza.vega@gmail.com>
2024-02-20 00:37:59 -0300

    Agregar elementos graficos para formulario y configuracion de API

--- src/components/webapp/ProfileUser/FormData.js
+++ src/components/webapp/ProfileUser/FormData.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Form, Input, Button, DatePicker } from "antd";
 import { AppContext} from "../../../App";
+import dayjs from "dayjs";
 
 const { Item } = Form;
 
@@ -8,6 +9,8 @@ const { Item } = Form;
 const FormData = () => {
     const { state, dispatch } = useContext(AppContext);
     console.log(state);
+    const dateFormat = 'YYYY/MM/DD';
+
 
   return (
     <Form initialValues={state.user}>
@@ -17,13 +20,13 @@ const FormData = () => {
       <Item name='last_name'>
         <Input placeholder="Apellido" />
       </Item>
-      <Item>
-        <DatePicker style={styles.dateBorn} placeholder="Fecha nacimiento" />
+      <Item >
+        <DatePicker defaultValue={[dayjs(state.user.date_of_birth, dateFormat), dayjs(state.user.date_of_birth, dateFormat)]} style={styles.dateBorn} placeholder="Fecha nacimiento" />
       </Item>
-      <Item>
+      <Item name='phone_number'>
         <Input placeholder="Teléfono" />
       </Item>
-      <Item>
+      <Item name='bio'>
         <Input.TextArea rows={4} placeholder="Descripción" />
       </Item>
       <Item>
